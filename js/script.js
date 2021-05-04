8e73eb16bb9fc3cdfe8228fed7fde94ba0b46807
kalidarkhan <kalidarkhan@gmail.com>
2021-05-04 17:36:44 +0600

    cataloglv1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        infinite: false,
-        slidesToShow: 2,
-        slidesToScroll: 2
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/carousel/pulse_left_solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/pulse_right_solid.png"></button>',
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
   });
