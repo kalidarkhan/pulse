f81b083546bfb8d857f4e59d9e405b9427f9c32b
kalidarkhan <kalidarkhan@gmail.com>
2021-06-22 11:44:45 +0600

    arrow-fixed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
     $('.carousel__inner').slick({
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/carousel/pulse_left_solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/pulse_right_solid.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/pulse_left_solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/pulse_right_solid.png"></button>',
         responsive: [
             {
                 breakpoint: 991,
